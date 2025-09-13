f5be97f6d9a7b6ef23ca69492c9edbfda58a6ca1
Jannah Kurniawati <jannahkurnia15@gmail.com>
2025-09-13 10:34:24 +0700

    edit citizen

--- src/components/user/CitizenScience.jsx
+++ src/components/user/CitizenScience.jsx
@@ -70,10 +70,10 @@ const CitizenScience = () => {
         if (showConfirmation) {
           return (
             <div className="p-4 border border-gray-300 rounded-lg flex flex-col items-center justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-24 h-24 text-ecotani-green">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-24 h-24 text-[#43703a]">
                 <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
               </svg>
-              <h3 className="text-3xl font-bold mt-4 text-ecotani-green">Data Sampah Berhasil Dikirim</h3>
+              <h3 className="text-3xl font-bold mt-4 text-[#43703a]">Data Sampah Berhasil Dikirim</h3>
               <p className="text-gray-700 mt-2">Data Anda akan membantu prediksi volume sampah.</p>
             </div>
           );
